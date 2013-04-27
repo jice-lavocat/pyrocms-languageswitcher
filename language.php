74e96c1d495ce96de39af64b1ff11a984cf41471
Antoine Benevaut <antoine@cavaencoreparlerdebits.fr>
2013-04-27 18:08:32 +0200

    v1.3.0 available on PyroCMS store https://www.pyrocms.com/store/details/language_plugin!

--- language.php
+++ language.php
@@ -1,73 +1,115 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Language plugin
- * 
+ * This is a language manager for PyroCMS
+ *
  * @author	Antoine Benevaut
- * 
- * @package	OursITShow
- * @subpackage  Lang
- * @category  	Plugin
+ * @date	24 sept 2012
+ * @website	http://www.cavaencoreparlerdebits.fr
+ * @website	http://languageswitcher.cavaencoreparlerdebits.fr
+ *
+ * @package	pyrocms-plugin
+ * @subpackage LanguageSwitcher Plugin
+ *
  */
+ 
 class Plugin_Language extends Plugin
 {
-  /**
-   * {{ language:switcher }}
-   *
-   * Language switcher allow to display selected site language as text or
-   * image flags and allow users to change current language.
-   *
-   * Optionals arguments:
-   *
-   * select mode :
-   * mode = txt | gif | png
-   *
-   * select display mode :
-   * display = inline
-   *
-   * {{ language:switcher mode='png' display='inline' }}
-   */
-  public function	switcher() {
-    $html		= null;
+	/**
+	 * Language switcher allow to display selected site language as text or
+	 * image flags and allow users to change current language.
+	 *
+	 * Options list :
+	 *
+	 * mode : select mode txt or gif or png
+	 * display : select display mode inline or none
+	 *
+	 * {{ language:switcher mode='png' display='inline' }}
+ 	 * {{ language:switcher mode='gif' }}
+ 	 *
+	 */
+	public function	switcher()
+	{
+		$html		= null;
 
-    $mode		= $this->attribute('mode', 'txt');
-    $display		= $this->attribute('display');
+		$mode		= $this->attribute('mode', 'txt');
+		$display	= $this->attribute('display');
 
-    $langs		= explode(",", Settings::get('site_public_lang'));
+		Asset::add_path('language', 'assets/language/');
+			
+		$this->template
+      		->append_css('language::language.css');
+      				
+		if ( ! ($html = $this->pyrocache->get('language-switcher-'.$mode.'-'.$display)))
+		{
+			$langs		= explode(",", Settings::get('site_public_lang'));
 
-    if (isset($langs) && !empty($langs)) {
-      $html .= "<style>";
-
-      if (isset($display) && (strcmp($display, 'inline') === 0)) {
-	$html .= "#language_switcher li { margin-right:2px;display:inline;list-style:none; }";
-      }
-      else {
-	$html .= "#language_switcher li { list-style:none; }";
-      }
-      unset($display);
-
-      $html .= "</style>";      
-      $html .= "<ul id='language_switcher'>";
-
-      foreach ($langs as $lang) {
-	$html .= "<li>";
-	$html .= "<a href='".current_url()."/?lang=".$lang."'>";
-	switch ($mode) {
-	case 'png':
-	  $html .= "<img src='".base_url()."/uploads/flags/png/".$lang.".png'/>";
-	  break;
-	case 'gif':
-	  $html .= "<img src='".base_url()."/uploads/flags/gif/".$lang.".gif'/>";
-	  break;
-	default:
-	  $html .= $lang;
+			if (isset($langs) && !empty($langs))
+			{     
+				if (isset($display) && (strcmp($display, 'inline') === 0))
+				{
+					$html .= '<ul id="language_switcher-inline">';
+				}
+				else
+				{
+					$html .= '<ul id="language_switcher">';
+				}
+      		
+				foreach ($langs as $lang)
+				{
+					$html .= "<li>";
+					$html .= "<a href='".current_url()."/?lang=".$lang."'>";
+				
+					switch ($mode)
+					{
+						case 'png':
+							$html .= Asset::img('language::png/'.$lang.'.png', $lang, array());
+						break;
+					
+						case 'gif':
+							$html .= Asset::img('language::gif/'.$lang.'.gif', $lang, array());
+						break;
+	
+						default:
+							$html .= $lang;
+					}
+				
+					$html .= '</a></li>';
+				}
+			
+				$html .= '</ul>';
+			}
+			
+			$this->pyrocache->write($html, 'language-switcher-'.$mode.'-'.$display, 1000);
+		}
+		return $html;
 	}
-	$html .= "</a></li>";
-      }
-      $html .= "</ul>";
-    }
-    return $html;
-  }
+	
+	/**
+	 * Language with google translate is a plugin to display dropdown options to allow users to translate
+	 * your website in any language they want !
+	 *
+	 * visit http://translate.google.com/manager/ for more explanation on google translate for your website.
+	 *
+	 *
+	 * Options list :
+	 *
+	 * container : this is the name of div id. It's used to make relation between HTML and JS.
+	 *             The default container name is : google_translate_element.
+	 *
+	 * {{ language:google_translate container='other_name' }}
+	 * {{ language:google_translate }}
+ 	 *
+	 */
+	public function google_translate()
+	{
+		$container		= $this->attribute('container', 'google_translate_element');
+		
+		return '<div id="'. $container .'"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: \''. CURRENT_LANGUAGE .'\'}, \''. $container .'\');
 }
-
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>';
+	}
+}
 /* End of file language.php */
