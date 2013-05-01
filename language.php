5dd312f1bbb30732871d5f39df58c22e59f1cb5d
Antoine Benevaut <antoine@cavaencoreparlerdebits.fr>
2013-05-01 15:03:43 +0200

    new display available : 'dropdown';

--- language.php
+++ language.php
@@ -37,51 +37,26 @@ class Plugin_Language extends Plugin
 
 		Asset::add_path('language', 'assets/language/');
 			
-		$this->template
-      		->append_css('language::language.css');
+		$this->template->append_css('language::language.css');
       				
-		if ( ! ($html = $this->pyrocache->get('language-switcher-'.$mode.'-'.$display)))
-		{
+		//if ( ! ($html = $this->pyrocache->get('language-switcher-'.$mode.'-'.$display)))
+		//{
 			$langs		= explode(",", Settings::get('site_public_lang'));
 
 			if (isset($langs) && !empty($langs))
 			{     
-				if (isset($display) && (strcmp($display, 'inline') === 0))
+				if ($display == 'dropdown')
 				{
-					$html .= '<ul id="language_switcher-inline">';
+					$html = $this->_switcher_dropdown($langs, $mode);
 				}
 				else
 				{
-					$html .= '<ul id="language_switcher">';
+					$html = $this->_switcher_list($langs, $mode, $display);
 				}
-      		
-				foreach ($langs as $lang)
-				{
-					$html .= "<li>";
-					$html .= "<a href='".current_url()."/?lang=".$lang."'>";
-				
-					switch ($mode)
-					{
-						case 'png':
-							$html .= Asset::img('language::png/'.$lang.'.png', $lang, array());
-						break;
-					
-						case 'gif':
-							$html .= Asset::img('language::gif/'.$lang.'.gif', $lang, array());
-						break;
-	
-						default:
-							$html .= $lang;
-					}
-				
-					$html .= '</a></li>';
-				}
-			
-				$html .= '</ul>';
 			}
 			
-			$this->pyrocache->write($html, 'language-switcher-'.$mode.'-'.$display, 1000);
-		}
+		//	$this->pyrocache->write($html, 'language-switcher-'.$mode.'-'.$display, 300);
+		//}
 		return $html;
 	}
 	
@@ -110,6 +85,75 @@ function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: \''. CURRENT_LANGUAGE .'\'}, \''. $container .'\');
 }
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>';
+	}
+
+	private function _switcher_list($langs, $mode, $display = null)
+	{
+		$html = null;
+
+		if (isset($display) && (strcmp($display, 'inline') === 0))
+		{
+			$html .= '<ul id="language_switcher-inline">';
+		}
+		else
+		{
+			$html .= '<ul id="language_switcher">';
+		}
+
+		foreach ($langs as $lang)
+		{
+			$html .= "<li>";
+			$html .= "<a href='".current_url()."?lang=".$lang."'>";
+			$html .= $this->_img_language($lang, $mode);
+			$html .= '</a></li>';
+		}
+
+		$html .= '</ul>';
+		return $html;
+	}
+
+	private function _switcher_dropdown($langs, $mode)
+	{
+		$html = '<div id="language_switcher" class="dropdown">';
+
+		$html .= '<script type="text/javascript">
+		function switchLanguage(sel) {
+		    var url = sel[sel.selectedIndex].value;
+		    window.location = "'.current_url().'?lang="+url;
+		}</script>';
+
+		$html .= $this->_img_language(CURRENT_LANGUAGE, $mode);
+
+		$html .= '<select name="language_switcher" class="" onchange="switchLanguage(this);">';
+
+		foreach ($langs as $lang)
+		{
+			$html .= '<option value="'.$lang.'"';
+			$html .= (CURRENT_LANGUAGE == $lang ? ' selected="selected" ' : ' ').'>';
+			$html .= $this->_img_language($lang, 'txt');
+		}
+
+		$html .= '</select>';
+		$html .= '</div><div style="clear:both"></div>';
+		return $html;
+	}
+
+	private function _img_language($lang, $mode)
+	{
+		$html = null;
+
+		switch ($mode)
+		{
+			case 'png':
+				$html = Asset::img('language::png/'.$lang.'.png', $lang, array());
+			break;
+			case 'gif':
+				$html = Asset::img('language::gif/'.$lang.'.gif', $lang, array());
+			break;
+			default:
+				$html = $lang;
+		}
+		return $html;
 	}
 }
 /* End of file language.php */
